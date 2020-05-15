d0f1ba7cdbc7e38950e7e3d99dc342874bdf74d0
Anil Gurung <anil_gurung15@yahoo.com>
2020-05-15 00:22:02 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,3 @@
-class Post < ActiveRecord::Base
-  validates :title, presence: true, clickbait: true
-  validates :content, length: { minimum: 250 }
-  validates :summary, length: { maximum: 250}
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-end 
-
 class ClickbaitValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
@@ -12,3 +5,12 @@ class ClickbaitValidator < ActiveModel::EachValidator
     end
   end
 end 
+
+class Post < ActiveRecord::Base
+  validates :title, presence: true, clickbait: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+end 
+
+
