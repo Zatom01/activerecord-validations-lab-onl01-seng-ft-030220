93806949a8a068e01835e49f9ae63bc48beff35c
Anil Gurung <anil_gurung15@yahoo.com>
2020-05-15 00:19:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,11 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  
-  validate :title_contains?
-  
-  @@keywords = [ /Won't Believe/i, /Secret/i, /Top [0-9]*/i,  /Guess/i]
 
-  def title_contains?
-    if !@@keywords.any? { |word| word.match title }
-     errors.add(:title, "Must be clickbait")
-    end
+class ClickbaitValidator < ActiveModel::EachValidator
+def validate_each(record, attribute, value)
+  unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
+    record.errors[attribute] << ("Not a clickbait title")
   end
+end
 end
